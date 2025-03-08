c500bcb4c8481ee2f380c4b1914dd5dabd0fa518
Quam <quambankole9@gmail.com>
2025-03-07 19:18:31 -0500

    woo-commerce theme function setup

--- themes/no-code-theme/functions.php
+++ themes/no-code-theme/functions.php
@@ -15,3 +15,14 @@ require_once get_theme_file_path( 'inc/hooks.php' );
 
  // Woo Commerce.
 require_once get_theme_file_path( 'inc/woocommerce.php' );
+
+
+function teapots_enqueue_woocommerce_styles() {
+    wp_enqueue_style(
+        'teapots-woocommerce-styles', 
+        get_theme_file_uri( '/assets/css/woocommerce.css' ),
+        array(), 
+        '1.0'    
+    );
+}
+add_action( 'wp_enqueue_scripts', 'teapots_enqueue_woocommerce_styles' );
